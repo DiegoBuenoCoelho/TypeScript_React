8d9c218706b8c6c081a69513ea9ffeba0553f1d6
Diego Bueno <diegobueno.dev@gmail.com>
2025-12-26 12:52:39 -0500

    Using Context for isRunning

--- proj03-timers/src/components/Header.tsx
+++ proj03-timers/src/components/Header.tsx
@@ -1,25 +1,25 @@
-import Button from './UI/Button.tsx';
-import { useTimersContext } from '../store/timers-context.tsx';
-import { MouseEvent } from 'react';
+import Button from "./UI/Button.tsx";
+import { useTimersContext } from "../store/timers-context.tsx";
+import { MouseEvent } from "react";
 
 export default function Header() {
+	const obTimerContext = useTimersContext();
 
-  const obTimerContext = useTimersContext();
+	const handleTimersClick = (event: MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+		if (obTimerContext.isRunning) {
+			obTimerContext.stopTimer();
+		} else {
+			obTimerContext.startTimers();
+		}
+	};
 
-  const handleTimersClick = (event: MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    if (obTimerContext.isRunning) {
-      obTimerContext.stopTimer();
-    } else {
-      obTimerContext.startTimers();
-    }
-  };
-
-  return (
-    <header>
-      <h1>ReactTimer</h1>
-
-      <Button onClick={handleTimersClick}>{obTimerContext.isRunning ? 'Stop Timers' : 'Start Timers'}</Button>
-    </header>
-  );
+	return (
+		<header>
+			<h1>ReactTimer</h1>
+			<Button onClick={handleTimersClick}>
+				{obTimerContext.isRunning ? "Stop Timers" : "Start Timers"}
+			</Button>
+		</header>
+	);
 }
--- proj03-timers/src/components/Timer.tsx
+++ proj03-timers/src/components/Timer.tsx
@@ -1,12 +1,56 @@
-import { FC } from 'react';
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
-import Container from './UI/Container.tsx';
+import { FC, useEffect, useRef, useState } from "react";
+import { useTimersContext, type Timer as TimerProps } from "../store/timers-context.tsx";
+import Container from "./UI/Container.tsx";
 
 export const Timer: FC<TimerProps> = ({ name, duration }) => {
+	const ONE_SECOND = 1000;
+	const refTimerInterval = useRef<number | null>(null);
+	const [thisCounter, setThisCounter] = useState(duration * ONE_SECOND);
+	const { isRunning } = useTimersContext();
+
+	console.log(thisCounter, refTimerInterval.current);
+	if (thisCounter <= 0 && refTimerInterval.current) {
+		console.log("executed A");
+		clearInterval(refTimerInterval.current);
+	}
+
+	useEffect(() => {
+		let thisTimer: number;
+		if (isRunning) {
+			thisTimer = setInterval(() => {
+				if (thisCounter > 0) {
+					setThisCounter((prevTime) => {
+						if (prevTime <= 0) {
+							return prevTime;
+						}
+						return prevTime - 100;
+					});
+				}
+			}, 100);
+
+			refTimerInterval.current = thisTimer;
+		} else {
+			if (refTimerInterval.current) {
+				clearInterval(refTimerInterval.current);
+			}
+		}
+
+		return () => clearInterval(thisTimer);
+	}, [isRunning]);
+
+	const formattedRemainingTime = (thisCounter / ONE_SECOND).toFixed(2);
+
 	return (
 		<Container as="article">
 			<h2>{name}</h2>
 			<p>Duration: {duration} seconds</p>
+			<p>
+				<progress
+					max={duration * 1000}
+					value={thisCounter}
+				/>
+			</p>
+			<p>{formattedRemainingTime}</p>
 		</Container>
 	);
 };
