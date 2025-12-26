a87616344ed1ed7e488f0a45d270173b6da3dbe3
Diego Bueno <diegobueno.dev@gmail.com>
2025-12-26 15:24:07 -0500

    Adding store and slice. Also, adding the provider to the APP

--- proj05-reduxCart/src/App.tsx
+++ proj05-reduxCart/src/App.tsx
@@ -1,21 +1,23 @@
-import Header from './components/Header.tsx';
-import Shop from './components/Shop.tsx';
-import Product from './components/Product.tsx';
-import { DUMMY_PRODUCTS } from './dummy-products.ts';
+import Header from "./components/Header.tsx";
+import Shop from "./components/Shop.tsx";
+import Product from "./components/Product.tsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.ts";
+import { Provider } from "react-redux";
+import { appStore } from "./store/store.ts";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Shop>
-        {DUMMY_PRODUCTS.map((product) => (
-          <li key={product.id}>
-            <Product {...product} />
-          </li>
-        ))}
-      </Shop>
-    </>
-  );
+	return (
+		<Provider store={appStore}>
+			<Header />
+			<Shop>
+				{DUMMY_PRODUCTS.map((product) => (
+					<li key={product.id}>
+						<Product {...product} />
+					</li>
+				))}
+			</Shop>
+		</Provider>
+	);
 }
 
 export default App;
---
+++ proj05-reduxCart/src/store/cart-slice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type CartItem = {
+	id: string;
+	title: string;
+	price: number;
+	qtty: number;
+};
+type CartState = {
+	items: CartItem[];
+};
+
+const initialState: CartState = {
+	items: [],
+};
+
+//------------------------ METHODS
+const addToCart = (
+	state: CartState,
+	action: PayloadAction<{ id: string; title: string; price: number }>
+) => {
+	//code
+	const itemIndex = state.items.findIndex((thisItem) => thisItem.id === action.payload.id);
+	if (itemIndex >= 0) {
+		state.items[itemIndex].qtty++;
+	} else {
+		state.items.push({ ...action.payload, qtty: 1 });
+	}
+};
+const removeFromCart = (state: CartState, action: PayloadAction<string>) => {
+	const itemIndex = state.items.findIndex((thisItem) => thisItem.id === action.payload);
+	if (itemIndex === 1) {
+		state.items.splice(itemIndex, 1);
+	} else {
+		state.items[itemIndex].qtty--;
+	}
+};
+//------------------------ SLICE DECLARATION
+export const cartSlice = createSlice({
+	name: "cart",
+	initialState,
+	reducers: {
+		addToCart,
+		removeFromCart,
+	},
+});
